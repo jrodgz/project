9c9bbe9adcbaf7d6bd89f534d632d581350ebff9
smilanko <smilanko@mantech.com>
2016-03-23 15:11:10 -0400

    - renamed the google js to visualization js for clarity; - added more namespaces for parsing; - added more global functions;

--- app/ArchiveVisualization/public_html/js/global.js
+++ app/ArchiveVisualization/public_html/js/global.js
@@ -2,6 +2,24 @@
  * only place global functions in here, but be limited
  */
 
+var parsingComplete = false;
+
 function updateStatus(divToUpdate, stringToSay) {
     divToUpdate.append('Action: ' + stringToSay + ' <br />');
 };
+
+function updateContent(divToUpdate, stringToSay) {
+    divToUpdate.append('Content: ' + stringToSay + ' <br />');
+};
+
+function setParsingComplete(isComplete) {
+    parsingComplete = isComplete;
+}
+
+function waitForParsingComplete(statusDiv) {
+    if(parsingComplete === false) {
+       window.setTimeout(function() {waitForParsingComplete(statusDiv)}, 100); /* this checks the flag every 100 milliseconds*/
+    } else {
+        updateStatus(statusDiv, 'INFO: Parsing complete');
+    }
+}
---
+++ app/ArchiveVisualization/public_html/js/visualization.js
@@ -0,0 +1,65 @@
+var visualization = visualization || {};
+
+// namespace for reading the google spreadsheet
+(function (google, $, undefined) {
+
+    var url;
+    var statusDiv;
+
+    google.setStatusDiv = function (div) {
+        statusDiv = div;
+    };
+
+    google.setSheetUrl = function (receivedUrl) {
+        url = receivedUrl;
+    };
+
+    google.loadSheet = function () {
+        if (!url) {
+            alert("provide a url to use first");
+        } else {
+            updateStatus(statusDiv, 'Loading sheet...');
+            return $.getJSON(url)
+                    .error(function () {
+                        updateStatus(statusDiv, 'FATAL: unable to reach spreadsheet');
+                    })
+                    .then(function (data) {
+                        updateStatus(statusDiv, 'Sheet loading complete');
+                        return data;
+                    });
+        }
+        return true;
+    };
+
+}(window.visualization.google = window.visualization.google || {}, jQuery));
+
+// namespace for parsing the json data, and stroing it into variables
+(function (parser, $, undefined) {
+
+    var mementos = [];
+    var tags = [];
+    
+    parser.getMementos = function () {
+        return mementos;
+    };
+
+    parser.getTags = function () {
+        return tags;
+    };
+
+    parser.addMemento = function (mementoSet) {
+        var numberOfMementos = mementoSet.length;
+        for (var i = 0; i < numberOfMementos; i++) {
+            mementos.push(mementoSet[i]);
+        }
+    };
+
+    parser.addTag = function (tagSet) {
+        var numberOfTags = tags.length;
+        for (var i = 0; i < numberOfTags; i++) {
+            tags.push(tagSet[i]);
+        }
+    };
+
+}(window.visualization.parser = window.visualization.parser || {}, jQuery));
+
