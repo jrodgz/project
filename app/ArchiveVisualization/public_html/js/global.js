f4270dbf59531cf789cacbddce5f5cde63b3da60
Joel Rodriguez <joel.d.rodriguez@navy.mil>
2016-03-24 08:41:13 -0400

    Adds function to create a zoomable group.

--- app/ArchiveVisualization/public_html/js/global.js
+++ app/ArchiveVisualization/public_html/js/global.js
@@ -23,3 +23,39 @@ function waitForParsingComplete(statusDiv) {
         updateStatus(statusDiv, 'INFO: Parsing complete');
     }
 }
+
+/**
+ * @brief Appends a pannable, zoomable container to the given group.
+ *
+ * @param[in] group
+ * The group in which the container will be appended.
+ *
+ * @param[in] width
+ * Width of input sensitive region.
+ *
+ * @param[in] height
+ * Height of input sensitive region.
+ */
+function createZoomableContainer(group, width, height) {
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 10])
+        .on('zoom', zoomed);
+
+    group.call(zoom);
+
+    var rect = group.append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .style('fill', 'none')
+        .style('pointer-events', 'all');
+
+    var container = group.append('g');
+
+    function zoomed() {
+        container.attr('transform',
+                       'translate(' + d3.event.translate + ')scale(' + 
+                       d3.event.scale + ')');
+    }
+
+    return container;
+}
