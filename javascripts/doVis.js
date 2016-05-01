612dd8d92b413386fba7d74286d91c1ce0435e0e
N0taN3rd <n0tan3rd@gmail.com>
2016-05-01 16:18:45 -0400

    static vis initial commit

---
+++ javascripts/doVis.js
@@ -0,0 +1,306 @@
+
+function createTopTenArchivingYears(where, data) {
+
+  var visualization = d3plus.viz()
+     .container(where)
+     .data(data)
+     .type("bar")
+     .id("Year of Archive")
+     .x("Year of Archive")
+     .y("Number Of Mementos")
+     .resize(true)
+     .draw()
+}
+
+function createTopTenKeywords(where, data) {
+
+  var visualization = d3plus.viz()
+     .container(where)
+     .data(data)
+     .type("bubbles")
+     .id(["group", "name"])
+     .depth(1)
+     .size("size")
+     .color("group")
+     .resize(true)
+     .draw()
+
+}
+
+/*
+ create chart top ten popular domains
+ */
+function createTopTenPopularDomains(where, data) {
+
+  var chart = c3.generate({
+    bindto: where,
+    data: {
+      x: 'x',
+      columns: [
+        data.x,
+        data.cols
+      ],
+      type: 'bar'
+    },
+    bar: {
+      width: {
+        ratio: 0.5
+      }
+    },
+    axis: {
+      rotated: true,
+      x: {
+        type: 'category',
+        show: true
+      }
+    }
+  });
+
+}
+
+function createZoomableContainer(where, data) {
+  var width = $(where).width();
+  var height = $(where).height();
+  var svg = d3.select(where)
+     .append('svg')
+     .attr('width', width)
+     .attr('height', height);
+
+  preprocess.onLoad(data);
+
+  var group = drawing.createZoomableContainer(svg, width, height);
+  drawing.drawGridLines(group, width, height, 100);
+  var view = new drawing.predefined.DynamicTextNodeView(
+     group, width, height, svg, d3.select(where));
+  view.drawControls($(where));
+}
+
+function createHistogramOfTags(where, data) {
+  c3.generate({
+    bindto: where,
+    data: {
+      x: 'x',
+      columns: [
+        data.x,
+        data.cols
+      ],
+      type: 'bar'
+    },
+    bar: {
+      width: {
+        ratio: 0.5
+      }
+    },
+    axis: {
+      x: {
+        type: 'category',
+        show: true
+      }
+    }
+  });
+}
+
+
+
+function yearMonthTagDomainTree(where, data) {
+  var visualization = d3plus.viz()
+     .container(where)
+     .data(data)
+     .type("tree_map")
+     .id(["year", "month", "tag", "domain"])
+     .size("num")
+     .time({"value": "year", "solo": data[0].year})
+     .tooltip({
+       fullscrean: true,
+       size: false,
+       large: 450,
+       small: 450,
+       value: ["year", "month", "tag", "domain", "dates"]
+     })
+     .resize(true)
+     .draw();
+}
+
+
+
+function tagDomainYearMonthDatGraph(where,data) {
+  var visualization = d3plus.viz()
+     .container(where)
+     .type("network")
+     .data(data.nodes)
+     .nodes({overlap: 0.5})
+     .edges(data.edges)
+     .size("size")
+     .id("id")
+     .tooltip(["connectionsOut", "connectionsIn"])
+     .descs({
+       "connectionsOut": "How many Tags/Domains/Dates this node points to",
+       "connectionsIn": "How many Tags/Domains/Dates point to this node"
+     })
+     .resize(true)
+     .draw();
+}
+
+function domainTagYearMonthTree(where, data) {
+  var visualization = d3plus.viz()
+     .container(where)
+     .data(data)
+     .type("tree_map")
+     .id([ "domain","tag","year", "month"])
+     .size("num")
+     .time({"value": "year", "solo": data[0].year})
+     .tooltip({
+       fullscrean: true,
+       size: false,
+       large: 450,
+       small: 450,
+       value: ["domain","tag","year", "month", "dates"]
+     })
+     .resize(true)
+     .draw();
+}
+
+function yearTagDomainYearMonthTree(where, data) {
+  var visualization = d3plus.viz()
+     .container(where)
+     .data(data)
+     .type("tree_map")
+     .id(["tag", "domain","year", "month"])
+     .size("num")
+     .time({"value": "year", "solo": data[0].year})
+     .tooltip({
+       fullscrean: true,
+       size: false,
+       large: 450,
+       small: 450,
+       value: ["tag", "domain","year", "month", "dates"]
+     })
+     .resize(true)
+     .draw();
+}
+
+function createTimeLine(where, tldata) {
+  var tl = new TimelineChart(where, tldata, {
+    tip: function (d) {
+      return d.urim + '<br>' +d.at || d.from + '<br>' + d.to;
+    },
+    ttip: function (d) {
+      let dit = S(d.label);
+      if (dit.contains(',')) {
+        return "Tags: " + dit.replaceAll(',', ',<br>').s;
+      } else {
+        return "Tags: " + d.s;
+      }
+    }
+  });
+
+}
+
+function createTimeLineOL(where, tldata) {
+  var tl = new TimelineChart(where, tldata, {
+    tip: function (d) {
+      return d.urim + '<br>' +d.at || d.tip +'<br>' +d.from + '<br>' + d.to;
+    },
+    ttip: function (d) {
+      let dit = S(d.label);
+      if (dit.contains(',')) {
+        return "Tags: " + dit.replaceAll(',', ',<br>').s;
+      } else {
+        return "Tags: " + d.s;
+      }
+    }
+  });
+
+}
+
+function numMtoOlap(where,data) {
+  var visualization = d3plus.viz()
+     .container(where)  // container DIV to hold the visualization
+     .data(data)  // data to use with the visualization
+     .type("scatter")    // visualization type
+     .id("tag")         // key for which our data is unique on
+     .x("nol")         // key for x-axis
+     .y("nm")        // key for y-axis
+     .draw();
+
+}
+
+
+function timeGraph(data) {
+  var len = data.tagedMs.length;
+  for(var i = 0; i <len; ++i){
+    d3plus.viz()
+       .container("#cchart"+(i+1))
+       .type("rings")
+       .data(data.tagedMs[i].nodes)
+       .id('name')
+       .edges({
+         "size": "strength",
+         "value": data.tagedMs[i].edges
+       })
+       .resize(true)
+       .focus(data.tags[i])
+       .draw();
+  }
+
+}
+
+
+function olapGraph(data) {
+  var len = data.tagedMs.length;
+  for(var i = 0; i <len; ++i){
+    d3plus.viz()
+       .container("#cchartt"+(i+1))
+       .type("rings")
+       .data(data.tagedMs[i].nodes)
+       .id('name')
+       .edges({
+         "size": "strength",
+         "value": data.tagedMs[i].edges
+       })
+       .resize(true)
+       .focus(data.tagedMs[i].focus)
+       .draw();
+  }
+
+
+}
+
+// d.domainTYMTree ;
+// d.tagDomainYearMonthTree ;
+// d.tagDomainYearMonthDatGraph ;
+var d =  getData();
+
+  d.timeline.forEach(function (i) {
+    i.data.forEach(function (ii) {
+      ii.at = new Date(ii.at);
+      ii.type = TimelineChart.TYPE.POINT;
+    });
+  });
+
+  olapGraph(d.tagOlapGraph);
+  createTimeLine('#chart1',d.timeline);
+  createTopTenArchivingYears('#topTenArchivingYears', d.topTenArchingYears );
+
+  createTopTenKeywords('#topTenPopularKeywords', d.topTenKeyWords);
+
+  createTopTenPopularDomains('#topTenPopularDomains', d.topTenDomains);
+
+  createHistogramOfTags('#chart3', d.tagHistogram);
+
+  createZoomableContainer('#chart2', d.parsedData);
+  yearMonthTagDomainTree('#chart4',d.yMTDTree );
+  domainTagYearMonthTree('#chart5',d.domainTYMTree);
+  yearTagDomainYearMonthTree('#chart6',d.tagDomainYearMonthTree);
+
+  timeGraph(d.graphData);
+
+  // numMtoOlap("#numMtoNumOlap",numMToOverlap );
+  // #numMtoTimeSpanned
+  //  #numMtoNumOlap
+
+  listenForClicks();
+  listenForClicks2(d.graphData.tagedMs.length);
+  listenForClicks3();
+
+
---
+++ javascripts/slider.js
@@ -0,0 +1,139 @@
+function listenForClicks() {
+   var currentChart = 1;
+   var numberOfChartsOnPage = $('.chart').length;
+   updatenavigationTextForArrows(currentChart);
+
+   $('.right').click(function () {
+      if (currentChart <= numberOfChartsOnPage) {
+         $("#box" + currentChart).hide(function () {
+            currentChart++;
+            updatenavigationTextForArrows(currentChart);
+         });
+      }
+
+   });
+   $('.left').click(function () {
+      if (currentChart > 1) {
+         $("#box" + (currentChart - 1)).show(function () {
+            currentChart--;
+            updatenavigationTextForArrows(currentChart);
+         });
+      }
+   });
+
+   function updatenavigationTextForArrows(currentChart) {
+      var messageForNextButton = "Nothing follows";
+      var messageForPreviousButton = "Nothing proceeds";
+      var nextChart = currentChart + 1;
+      var previousChart = currentChart - 1;
+      if (nextChart <= numberOfChartsOnPage) {
+         $('#next_chart_container').css('visibility', 'visible');
+         messageForNextButton = $('#chart' + nextChart).attr('data-chart-desc');
+         $('#next_chart').text(messageForNextButton);
+      } else {
+         $('#next_chart_container').css('visibility', 'hidden');
+      }
+      if (previousChart >= 1) {
+         $('#prev_chart_container').css('visibility', 'visible');
+         messageForPreviousButton = $('#chart' + previousChart).attr('data-chart-desc');
+         $('#prev_chart').text(messageForPreviousButton);
+      } else {
+         $('#prev_chart_container').css('visibility', 'hidden');
+      }
+   }
+}
+
+function listenForClicks2(num) {
+   // by default, chart1 is shown
+   var currentChart = 1;
+   var numberOfChartsOnPage = $('.chart2').length;
+   console.log(numberOfChartsOnPage);
+   updatenavigationTextForArrows2(currentChart);
+
+   $('.right2').click(function () {
+      if (currentChart < numberOfChartsOnPage) {
+         $("#bbox" + currentChart).hide(function () {
+            currentChart++;
+            updatenavigationTextForArrows2(currentChart);
+         });
+      }
+
+   });
+   $('.left2').click(function () {
+      if (currentChart > 1) {
+         $("#bbox" + (currentChart - 1)).show(function () {
+            currentChart--;
+            updatenavigationTextForArrows2(currentChart);
+         });
+      }
+   });
+
+   function updatenavigationTextForArrows2(currentChart) {
+      var messageForNextButton = "Nothing follows";
+      var messageForPreviousButton = "Nothing proceeds";
+      var nextChart = currentChart + 1;
+      var previousChart = currentChart - 1;
+      if (nextChart <= numberOfChartsOnPage) {
+         $('#next_chart_container2').css('visibility', 'visible');
+         messageForNextButton = $('#cchart' + nextChart).attr('data-chart-desc');
+         $('#next_chart2').text(messageForNextButton);
+      } else {
+         $('#next_chart_container2').css('visibility', 'hidden');
+      }
+      if (previousChart >= 1) {
+         $('#prev_chart_container2').css('visibility', 'visible');
+         messageForPreviousButton = $('#cchart' + previousChart).attr('data-chart-desc');
+         $('#prev_chart2').text(messageForPreviousButton);
+      } else {
+         $('#prev_chart_container2').css('visibility', 'hidden');
+      }
+   }
+}
+
+
+function listenForClicks3() {
+   // by default, chart1 is shown
+   var currentChart = 1;
+   var numberOfChartsOnPage = $('.chart3').length;
+   console.log(numberOfChartsOnPage);
+   updatenavigationTextForArrows2(currentChart);
+
+   $('.right3').click(function () {
+      if (currentChart < numberOfChartsOnPage) {
+         $("#bboxx" + currentChart).hide(function () {
+            currentChart++;
+            updatenavigationTextForArrows2(currentChart);
+         });
+      }
+
+   });
+   $('.left3').click(function () {
+      if (currentChart > 1) {
+         $("#bboxx" + (currentChart - 1)).show(function () {
+            currentChart--;
+            updatenavigationTextForArrows2(currentChart);
+         });
+      }
+   });
+
+   function updatenavigationTextForArrows2(currentChart) {
+      var messageForNextButton = "Nothing follows";
+      var messageForPreviousButton = "Nothing proceeds";
+      var nextChart = currentChart + 1;
+      var previousChart = currentChart - 1;
+      if (nextChart <= numberOfChartsOnPage) {
+         $('#next_chart_container3').css('visibility', 'visible');
+         messageForNextButton = $('#cchartt' + nextChart).attr('data-chart-desc');
+         $('#next_chart3').text(messageForNextButton);
+      } else {
+         $('#next_chart_container3').css('visibility', 'hidden');
+      }
+      if (previousChart >= 1) {
+         $('#prev_chart_container3').css('visibility', 'visible');
+         messageForPreviousButton = $('#cchartt' + previousChart).attr('data-chart-desc');
+         $('#prev_chart3').text(messageForPreviousButton);
+      } else {
+         $('#prev_chart_container3').css('visibility', 'hidden');
+      }
+   }
+}
